4fd32876409efbc3ab14d535d6b743c26e32ddc3
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-05-16 17:01:54 -0400

    add header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import AppBar from '@material-ui/core/AppBar'
+
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar'
-import MenuIcon from '@material-ui/core/Menu'
+import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
 
 
@@ -9,21 +10,28 @@ import IconButton from '@material-ui/core/IconButton'
 class Header extends Component {
     render() {
         return (
-           <AppBar
-            position = 'fixed'
-            style = {{
-                backgroundColor: '#2f2f2f',
-                boxShadow: 'None',
-                padding: '10px 0px'
-            }}
-           >
-               <Toolbar>
+            <AppBar 
+                position = 'fixed'
+                style = {{
+                    backgroundColor: '#2f2f2f',
+                    boxShadow: 'None',
+                    padding: '10px, 0px'
+                }}
+            >
+                <Toolbar>
                    <div className='header_logo'>
                         <div className='font_righteous header_logo_venue'>The Venue</div>
                         <div className='header_logo_title'>Musical Events</div>
                    </div>
-               </Toolbar>
-           </AppBar>
+                   <IconButton
+                        aria-label='Menu'
+                        color='inherit'
+                        onClick={() => console.log('open')}
+                   >
+                       <MenuIcon />
+                   </IconButton>
+                </Toolbar>
+            </AppBar>
         )
     }
 }
